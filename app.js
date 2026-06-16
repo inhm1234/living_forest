8410a32d64c2d4a42495460a3b2d4e052d459142
inhm1234 <a01063173324@gmail.com>
2026-06-16 12:25:19 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-// 살아있는 숲 V1.73.9 world tree big scale + class fix hotfix
+// 살아있는 숲 V1.73.10 my tree upsize hotfix
 // 프로젝트명: 살아있는 숲
-// 버전명: V1.73.9 world tree big scale + class fix hotfix
+// 버전명: V1.73.10 my tree upsize hotfix
 // 목적: 전체숲 시간대별 전용 배경 이미지를 연결하고 오버레이 실험을 원복
 // 저장 방식: localStorage + Google Sheets friend_seats/friend_links 연동
 // 저장 방식: localStorage 유지
 
 const APP_CONFIG = {
   name: "살아있는 숲",
-  version: "V1.73.9 world tree big scale + class fix hotfix",
+  version: "V1.73.10 my tree upsize hotfix",
   dataSchemaVersion: 12,
   baseStorageKey: "livingForestV012",
   testStorageKey: "livingForestV012_TEST",
